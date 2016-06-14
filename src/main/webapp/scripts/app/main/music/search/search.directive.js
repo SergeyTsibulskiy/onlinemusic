ff8c12f10b2e80be2eabc9fbc29c47d94ccead88
c4r0n0s <c4r0n0s@gmail.com>
2016-06-15 00:02:41 +0300

    add NON select item

--- src/main/webapp/scripts/app/main/music/search/search.directive.js
+++ src/main/webapp/scripts/app/main/music/search/search.directive.js
@@ -16,10 +16,12 @@ angular.module('onlinemusicApp')
             $scope.predicate = 'id';
             $scope.reverse = true;
             $scope.page = 0;
+            var notSelected = 'Не вибрано';
 
             $scope.loadGenres = function () {
                 if ($scope.genres.length == 0) {
-                    $scope.genres.push('');
+
+                    $scope.genres.push({id: '99999', name: notSelected});
                     genreService.query({page: $scope.page, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
                         for (var i = 0; i < result.length; i++) {
                             $scope.genres.push(result[i]);
@@ -31,7 +33,7 @@ angular.module('onlinemusicApp')
             $scope.loadAlbums = function () {
                 if ($scope.albums.length == 0) {
                     albumService.query({page: $scope.page, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
-                        $scope.albums.push('');
+                        $scope.albums.push({id: '99999', name: notSelected});
                         for (var i = 0; i < result.length; i++) {
                             $scope.albums.push(result[i]);
                         }
@@ -42,7 +44,7 @@ angular.module('onlinemusicApp')
             $scope.loadArtists = function () {
                 if ($scope.artists.length == 0) {
                     artistService.query({page: $scope.page, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
-                        $scope.artists.push('');
+                        $scope.artists.push({id: '99999', name: notSelected});
                         for (var i = 0; i < result.length; i++) {
                             $scope.artists.push(result[i]);
                         }
@@ -51,9 +53,9 @@ angular.module('onlinemusicApp')
             };
 
             $scope.search = function (form) {
-                var album = _.isUndefined(form.album) ? '' : form.album.name;
-                var artist = _.isUndefined(form.artist) ? '' : form.artist.name;
-                var genre = _.isUndefined(form.genre) ? '' : form.genre.name;
+                var album = _.isUndefined(form.album) || form.album === notSelected ? '' : form.album.name;
+                var artist = _.isUndefined(form.artist)|| form.artist === notSelected ? '' : form.artist.name;
+                var genre = _.isUndefined(form.genre) || form.genre === notSelected ? '' : form.genre.name;
                 var query = _.isUndefined(form.query) ? '' : form.query;
                 musicService.search({query: query, artist: artist, album: album, genre: genre }, function (result, headers) {
                     $scope.musics = [];
